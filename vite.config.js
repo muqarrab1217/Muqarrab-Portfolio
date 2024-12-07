647a0e46f261cbd32abeffba5a38325a9ed62b0e
unknown <muq1217@gmail.com>
2024-12-08 01:19:46 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/',
-})
-
-
+  base: '/Muqarrab-Portfolio/', // Replace with your GitHub repository name
+});
